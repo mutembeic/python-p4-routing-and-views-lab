8ef594c6955c1258940a27ce80f415c01192446b
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-09-27 11:18:02 +0300

    update

--- app/app.py
+++ app/app.py
@@ -53,38 +53,4 @@ if __name__ == '__main__':
     app.run()
 
 
-# from flask import Flask, request
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return '<h1>Python Operations with Flask Routing and Views</h1>'
-
-# @app.route('/print/<parameter>')
-# def print_parameter(parameter):
-#     return parameter
-
-# @app.route('/count/<int:parameter>')
-# def count(parameter):
-#     result = '\n'.join(str(i) for i in range(parameter))
-#     return result
-
-# @app.route('/math/<int:num1>/<operation>/<int:num2>')
-# def math(num1, operation, num2):
-#     if operation == '+':
-#         result = num1 + num2
-#     elif operation == '-':
-#         result = num1 - num2
-#     elif operation == '*':
-#         result = num1 * num2
-#     elif operation == 'div':
-#         result = num1 / num2
-#     elif operation == '%':
-#         result = num1 % num2
-#     else:
-#         return 'Invalid operation'
-#     return str(result)
-
-# if __name__ == '__main__':
-#     app.run()
+ 
